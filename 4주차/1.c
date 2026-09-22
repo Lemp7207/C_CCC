ee3bba49b845ec028cf660688de5f39e2b80bcc8
develMake_47 <130072951+Lemp7207@users.noreply.github.com>
2026-09-22 19:13:25 +0900

    Add files via upload

---
+++ 4주차/1.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() 
+{
+    int num;
+    int count = 0;
+    int r;
+
+    srand(time(NULL));
+
+    r = rand() % 100 + 1;
+
+    printf("1부터 100사이에 숫자 맞춰보기");
+
+    while(1)
+    {
+        printf("숫자 입력하기 : ");
+        scanf("%d", &num);
+        count += 1;
+
+        if (num > r)
+        {
+            printf("입력한 숫자가 높습니다.");
+        }
+        else if (num < r)
+        {
+            printf("입력한 숫자가 낮습니다.");
+        }
+        else
+        {
+            printf("정답입니다.");
+            printf("%d번 반복만에 맞췄습니다.\n", count);
+            break;
+        }
+    }
+}
---
+++ 4주차/3.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int p(int n)
+{
+    if (n==0)
+    {
+        return;
+    }
+    p(n-1);
+    printf("%d\n", n);
+}
+
+int main()
+{
+    int n;
+
+    printf("숫자 입력 : ");
+    scanf("%d", &n);
+    p(n);
+    return 0;
+}
